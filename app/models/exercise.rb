972d6ef9c5b692da535abfb13f127cbfa3b6f0db
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-10 23:56:51 +0200

    improved model tests

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,7 @@
 class Exercise < ApplicationRecord
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :repeat, presence: true
+	validates :calories, presence: true
+	validates :duration, presence: true
 end
--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Should not save exercise without name description" do
+   	exercise = Exercise.new
+	assert_not exercise.save
+   end
+   
+   test "Should save exercise when all data is provided" do
+   	exercise = Exercise.new
+	exercise.name = 'Foo'
+	exercise.description = 'Foo'
+	exercise.duration = 10
+	exercise.calories = 2.5
+	exercise.repeat = 1
+	assert exercise.save
+   end
+
+   test "Should work with fixture data" do
+  	exercise = exercises(:one)
+    assert exercise.save
+	assert exercise.id
+   end
 end
