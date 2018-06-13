013d6a2bed669bd8b7485c3255cf4088138c31eb
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-13 23:37:48 +0200

    Added exercise

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
+import Splash from './Splash';
 import NotFound  from './NotFound';
 import Exercise  from './Exercise';
 import About  from './About';
@@ -16,12 +17,13 @@ class App extends Component {
               <h1>Exercises</h1>
             </div>
             <div>
-              <Link to="/">Home</Link> | 
+              <Link to="/exercises">Exercises</Link> | 
               <Link to="/about">About</Link> | 
             </div>
             <Switch>
-              <Route exact path='/' component={Exercise}/>  
+              <Route exact path='/' component={Splash}/>  
               <Route exact path='/404' component={NotFound}/> 
+              <Route exact path='/exercises' component={Exercise} />
               <Route exact path='/about' component={About} />
               <Route component={NotFound} />
           </Switch>
