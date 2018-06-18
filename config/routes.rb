7781b7a733e9cbc39b829e45aebc42186d70f423
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-18 23:54:11 +0200

    ok, done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	get 'home/index'
+#	option 'api/exercises' => 'exercises#index'
 	scope "/api", as: "user" do
 		resources :exercises
 	end
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 const url = (process.env.NODE_ENV === 'development') 
-    ? 'http://localhost:5000'
+    ? 'http://localhost:3000'
 	: 'https://calories-burner.herokuapp.com';
 		
 const headers =  {
