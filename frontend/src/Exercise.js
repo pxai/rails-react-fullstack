300fcc3976980d8f6a5de85fbe5b0d62763dcd56
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-06-20 14:23:27 +0200

    ok, done

--- frontend/src/Exercise.js
+++ frontend/src/Exercise.js
@@ -1,4 +1,42 @@
 import React, { Component } from 'react';
+import './Exercise.css';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Switch from '@material-ui/core/Switch';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+
+const classes = {
+card: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+    height: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: 1,
+    paddingBottom: 1,
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  }
+};
 
 class Exercise extends Component {
 
@@ -7,12 +45,41 @@ class Exercise extends Component {
 	  this.setState({ selected: false});
   }
 
+  handleChange(item) {
+		console.log('Changed: ', item)	  
+  }
+
   render() {
 	  const exercise = this.props.exercise;
     return (
-		<div>
-			<h3>Exercise: {exercise.name}</h3>
-			<div><p>{exercise.description}</p></div>
+		<div className="Exercise">
+<Card className={classes.card}>
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <Typography variant="headline">{exercise.name}</Typography>
+            <Typography variant="subheading" color="textSecondary">
+				{exercise.description}
+            </Typography>
+          </CardContent>
+          <div className={classes.controls}>
+			<Switch
+			          checked={true}
+					            onChange={this.handleChange('checkedA')}
+								          value="checkedA"
+										          />
+            <Typography variant="subheading" color="textSecondary">
+			<span><b>Repeat:</b>{exercise.repeat}</span>
+			                  <span><b>Duration:</b>{exercise.duration}</span>
+			                  <span><b>Calories:</b>{exercise.calories}</span>
+            </Typography>
+          </div>
+        </div>
+        <CardMedia
+          className={classes.cover}
+          image="/static/images/cards/live-from-space.jpg"
+          title="Live from space album cover"
+        />
+      </Card>
 		</div>
     );
   }
